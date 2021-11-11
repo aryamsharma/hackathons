2d7e1d4e24f96ce544bdc9c02fb01d827f6f8c14
aryamsharma <aryamsharma@outlook.com>
2021-11-11 02:24:35 -0500

    quicksave

--- Hackit_Shipit_2020/main.py
+++ Hackit_Shipit_2020/main.py
@@ -4,6 +4,7 @@ from time import sleep
 import pygame.freetype
 import math
 
+
 class Board:
     def __init__(self, bg_color, background_name="ocean1.jpg"):
         pygame.display.set_caption("lily")
@@ -21,8 +22,8 @@ class Board:
         off_y = -25
         for text in help_text:
             off_y += 30
-            self.font.render_to(self.screen,(5, off_y), text, Black)
-    
+            self.font.render_to(self.screen, (5, off_y), text, Black)
+
     def _is_dead(self, obj):
         x, y = obj[0]
         if x <= 0 or y <= 0 or x >= 1280 or y >= 720:
@@ -46,58 +47,58 @@ class Board:
 
             for obj in self.all_birds:
                 ret = Board._is_dead(self, obj)
-                
-                if ret == True:
+
+                if ret is True:
                     self.to_delete.append(obj)
                     continue
 
                 Board._draw_bird(self, self.screen, obj)
-            
+
             for event in pygame.event.get():
                 screen.events(event)
 
             pygame.display.flip()
-            sleep(fps_time) # 1000 / fps = time between each frame (millisec)
+            sleep(fps_time)  # 1000 / fps = time between each frame (millisec)
             Board.reset(self)
 
     def reset(self):
         for obj in self.to_delete:
             self.all_birds.remove(obj)
-        
+
         del self.to_delete[:]
 
     def _create_lily(pos):
         noise_range = 50
         return pos, noise_range
-    
+
     def _create_bird(self):
         while True:
             pos = [random.randint(50, 1230), random.randint(50, 670)]
             direction = random.choice([
-                [-1, 0], [0, 1], [1, 0], [0, -1], 
+                [-1, 0], [0, 1], [1, 0], [0, -1],
                 [1, 1], [-1, -1], [1, -1], [-1, 1]])
-            
+
             ret = Board._is_dead(self, (pos, direction))
             if ret:
                 continue
             break
         return pos, direction
-    
+
     def _del_obj(head):
         if len(head) >= 1:
             head.pop()
         else:
             print("nothing to remove")
-    
+
     def _fps_to_sec(fps):
         return (1000 / fps) / 1000
-    
+
     def _draw_lily(screen, obj):
         loc = obj[0]
         noise_range = obj[1]
         pygame.draw.circle(screen, D_Green, loc, 10, 0)
         pygame.draw.circle(screen, Red, loc, noise_range, 2)
-    
+
     def _draw_bird(self, screen, obj):
         x, y = obj[0]
         x_diff, y_diff = obj[1]
@@ -112,11 +113,10 @@ class Board:
                     obj[1][1] = y_diff
 
         x, y = x + x_diff, y + y_diff
-        
+
         pygame.draw.circle(screen, Gray, (x, y), 10, 0)
         obj[0][0] = x
         obj[0][1] = y
-
 
     def events(self, event):
         if event.type == pygame.KEYDOWN:
@@ -135,15 +135,16 @@ class Board:
             elif event.key in [pygame.K_q, pygame.K_ESCAPE]:
                 print("q or esc")
                 exit()
-        
+
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print("mouse down")
             pos = pygame.mouse.get_pos()
             self.all_lilys.append(Board._create_lily(pos))
-        
+
         elif event.type == pygame.QUIT:
             print("x")
             exit()
+
 
 if __name__ == "__main__":
     Black = (0, 0, 0)
@@ -157,7 +158,8 @@ if __name__ == "__main__":
 
     pygame.init()
     help_text = [
-        "use your mouse left click to spawn B.P.M, l to delete the most recent B.P.M!",
+        """Use your mouse left click to spawn B.P.M,
+         l to delete the most recent B.P.M!""",
         "b to spawn a bird, k to delete the most recent bird!",
         "Q to quit or press the x on the top left!"]
 
